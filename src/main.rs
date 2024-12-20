b5fc080f7eae9aafae40267257fce1569941aad1
Tahir <raforawesome@raforaweso.me>
2024-12-19 23:36:59 -0500

    Set up basic web server

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
-fn main() {
-    println!("Hello, world!");
+//! The music server backend for [insert name here].
+
+use axum::{Router, routing::get};
+
+const IP_ADDR: &str = "0.0.0.0:31078";
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new()
+        .route("/", get(root_responder));
+
+    let listener = tokio::net::TcpListener::bind(IP_ADDR).await.unwrap();
+    axum::serve(listener, app).await.unwrap();
+}
+
+async fn root_responder() -> &'static str {
+    "Hello, world!"
 }
