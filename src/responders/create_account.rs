3af17e6db8af7edbea43a6aa5057f295b49d9187
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-12-30 17:49:03 -0500

    Implement `create_account` API endpoint

--- src/responders/create_account.rs
+++ src/responders/create_account.rs
@@ -1,12 +1,23 @@
 use axum::body::Bytes;
 use serde::Deserialize;
 
+use crate::services::AccountService;
+
+use super::BadRequestError;
+
 #[derive(Deserialize)]
 struct CreateAccount {
-    name: String,
+    username: String,
     password: String,
+    is_admin: bool,
 }
 
-pub async fn create_account(bytes: Bytes) {
-    todo!()
+pub async fn create_account(bytes: Bytes) -> Result<(), BadRequestError> {
+    let request_info: CreateAccount = bincode::deserialize(&bytes)?;
+    AccountService.register(
+        request_info.username,
+        request_info.password,
+        request_info.is_admin,
+    )?;
+    Ok(())
 }
