182f4dec498e6b0cbf40a27a436dc5891608d6a6
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-12-29 23:08:57 -0500

    Create `db::accounts::AccountRecord` to store additional information

--- src/db/accounts.rs
+++ src/db/accounts.rs
@@ -9,13 +9,29 @@ use std::{
     sync::LazyLock,
 };
 
+/// A global concurrent hashmap storing a list of all accounts read in from disk.
+///
+/// TODO:
+/// - Consider changing to a frozen map
 pub static ACCOUNTS: LazyLock<DashMap<String, String>> = LazyLock::new(DashMap::new);
+
+/// A small data struct to hold information about an account. The reason usernames are not
+/// included in this record is we currently use usernames as the key to the account_db, rather than the
+/// more typical implementation which would be user id. The rationale behind this is that we simply
+/// do not need elegant account tracking as each server is most likely not going to be used by a
+/// large amount of people, given the decentralized nature of Orpheus.
+#[derive(Serialize, Deserialize)]
+pub struct AccountRecord {
+    password_hash: String,
+    /// can the user manage the server (i.e. create new accounts?)
+    is_admin: bool,
+}
 
 #[derive(Serialize, Deserialize)]
 pub struct AccountsManager {
     #[serde(skip)]
     path: PathBuf,
-    accounts: DashMap<String, String>,
+    accounts: DashMap<String, AccountRecord>,
 }
 
 unsafe impl Send for AccountsManager {}
@@ -48,6 +64,13 @@ impl AccountsManager {
     }
 }
 
+/// # Why manually implement drop for this type?
+/// There's a lot of solutions to solve the problem of "when exactly do we save?"
+/// Typically the solution reached is allowing manual saving + auto-saving at
+/// every set interval or action. However, since Rust guarantees it will call
+/// every struct's drop implementation, on graceful exit or on panic, we can
+/// simply just delegate the saving to disk then. Ideally, we'll implement more frequent
+/// saves than that to account for power outages though.
 impl Drop for AccountsManager {
     fn drop(&mut self) {
         self.save();
