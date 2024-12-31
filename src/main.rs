11d2c17a4c85bf3d8712778de4a020e9b3af65ad
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-12-30 21:17:07 -0500

    Add off-thread loop to auto-save accounts when necessary

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,17 @@
 //! the main.rs file contains the binary part of the application, i.e.
 //! the code for the main function and any relevant details.
 
+use std::time::Duration;
+
 use axum::{
     http::StatusCode,
     routing::{get, post},
     Router,
 };
 use tower_http::{cors::CorsLayer, trace::TraceLayer};
-use tracing::{info, Level};
+use tracing::{info, trace, Level};
 // import exports defined in `src/lib.rs`:
-use orpheus::responders;
+use orpheus::{responders, services::AccountService};
 
 // we want port 31078 over all interfaces (0.0.0.0)
 const IP_ADDR: &str = "0.0.0.0:31078";
@@ -23,6 +25,7 @@ async fn main() {
         // .without_time()
         .init();
 
+    AccountService.verify();
     let app = Router::new()
         .layer(TraceLayer::new_for_http()) // makes debugging in async frameworks tear-free!
         .layer(CorsLayer::permissive()) // idk what cors even does but it ruins my life
@@ -32,6 +35,16 @@ async fn main() {
             get(async || StatusCode::METHOD_NOT_ALLOWED),
         ) // explicitly disallow get requests as we need binary data
         .route("/create-account", post(responders::create_account));
+
+    std::thread::spawn(|| loop {
+        // automatically save accounts structs if needed
+        let account_service = AccountService.as_ref();
+        if account_service.is_dirty() {
+            trace!("accounts service is marked dirty, autosaving...");
+            account_service.save();
+        }
+        std::thread::sleep(Duration::from_secs(1));
+    });
 
     let listener = tokio::net::TcpListener::bind(IP_ADDR).await.unwrap();
     info!("Listening on {}...", IP_ADDR);
