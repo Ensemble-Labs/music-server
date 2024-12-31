82e94b6cd53c63869a0a70cca7189bfa2b855658
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2024-12-30 19:06:10 -0500

    Create new service `crate::services::Config`

--- src/config.rs
+++ src/config.rs
@@ -1,13 +1,64 @@
-use serde::Deserialize;
+use std::{path::PathBuf, sync::LazyLock};
+
+use serde::{Deserialize, Serialize};
+use tokio::sync::RwLock;
 
 /// Rust representation of the TOML config for Orpheus.
 /// Example can be found in `orpheus-EXAMPLE.toml`.
-#[derive(Deserialize)]
+#[derive(Serialize, Deserialize, Debug)]
 pub struct Config {
     server: ServerConfig,
 }
 
-#[derive(Deserialize)]
+impl Config {
+    // Getters & Setters
+    pub fn server(&self) -> &ServerConfig {
+        &self.server
+    }
+
+    pub fn server_mut(&mut self) -> &mut ServerConfig {
+        &mut self.server
+    }
+}
+
+impl Config {
+    // Methods
+    pub fn save(&self) {
+        let path: PathBuf = std::env::current_dir()
+            .expect("Failed to access current directory!")
+            .join("orpheus.toml");
+        std::fs::write(
+            &path,
+            toml::to_string_pretty(&self).expect("Failed to serialize Config!"),
+        )
+        .expect("Failed to write to `orpheus.toml`!");
+    }
+
+    pub fn output(&self) -> String {
+        toml::to_string_pretty(&self).expect("Failed to serialize Config!")
+    }
+}
+
+#[derive(Serialize, Deserialize, Debug)]
 pub struct ServerConfig {
     data_path: String,
 }
+
+impl ServerConfig {
+    pub fn data_path(&self) -> &str {
+        &self.data_path
+    }
+
+    pub fn set_data_path(&mut self, data_path: String) {
+        self.data_path = data_path;
+    }
+}
+
+// Global config store from file
+pub static CONFIG: LazyLock<RwLock<Config>> = LazyLock::new(|| {
+    let path: PathBuf = std::env::current_dir()
+        .expect("Failed to access current directory!")
+        .join("orpheus.toml");
+    let toml_file: String = std::fs::read_to_string(&path).expect("Failed to read `orpheus.toml`!");
+    RwLock::new(toml::from_str(&toml_file).expect("`orpheus.toml` format error!"))
+});
--- src/lib.rs
+++ src/lib.rs
@@ -1,28 +1,21 @@
 //! The lib.rs file is the crate's library root and is used to organize
 //! the crate's namespace, hold unit tests, andprovide utility functions/macros.
 
-use std::sync::{Arc, LazyLock};
-
 // declare rust module tree
 pub mod auth;
 pub mod config;
 pub mod db;
 pub mod responders;
 
-pub static CONFIG: LazyLock<Arc<config::Config>> = LazyLock::new(|| {
-    Arc::new(
-        toml::from_str(include_str!("../orpheus-EXAMPLE.toml")).expect("Malformed `orpheus.toml`!"),
-    )
-});
-
 // re-export commonly used types closer to crate root
 pub mod types {
     pub use crate::db::accounts::AccountRecord;
-    pub use crate::responders::BadRequestError;
+    pub use crate::responders::RequestResult;
 }
 
 // re-export all services for ease of use
 pub mod services {
+    pub use crate::config::CONFIG as Config;
     pub use crate::db::accounts::AccountService;
 }
 
@@ -37,5 +30,19 @@ mod tests {
             .parse::<Table>()
             .unwrap();
         dbg!(cfg_toml);
+    }
+
+    #[test]
+    pub fn test_read_config() {
+        let cfg = crate::config::CONFIG.blocking_read();
+        dbg!(cfg.server().data_path());
+        let _ = dbg!(cfg);
+    }
+
+    #[test]
+    pub fn test_change_config() {
+        let mut cfg = crate::config::CONFIG.blocking_write();
+        cfg.server_mut().set_data_path("data_path".into());
+        std::fs::write("./orpheus-out.toml", cfg.output()).unwrap();
     }
 }
