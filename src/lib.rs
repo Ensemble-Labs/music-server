605c3c357fc2881b3d104a5efb49269a319480a9
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-01-01 16:43:06 -0500

    Create `make_getters!` macro for structs in `src/lib.rs`

--- src/lib.rs
+++ src/lib.rs
@@ -18,6 +18,39 @@ pub mod services {
     pub use crate::db::accounts::AccountService;
 }
 
+/// A macro to generate getters for every field of a struct.
+///
+/// Example:
+/// ```rs
+/// pub struct MyStruct {
+///     field1: String,
+///     field2: i32,
+/// }
+/// make_getters!(MyStruct, field1: String, field2: i32);
+/// ```
+macro_rules! make_getters {
+    ($s:ident,$($x:ident:$t:ty),+) => { // dispatcher using recursion to emulate switch
+        impl $s {
+            $(make_getters!{ $x:$t })*
+        }
+    };
+    ($x:ident:String) => { // special case 1: if referencing a `String`, return `&str` not `&String`
+        pub fn $x(&self) -> &str {
+            &self.$x
+        }
+    };
+    ($x:ident:Vec<$t:ty>) => { // special case 2: instead of `&Vec<T>`, return `&[T]`
+        pub fn $x(&self) -> &[$t] {
+            &self.$x
+        }
+    };
+    ($x:ident:$t:ty) => { // default case: just return `&T`
+        pub fn $x(&self) -> &$t {
+            &self.$x
+        }
+    };
+}
+
 // unit testing
 #[cfg(test)]
 mod tests {
