9f2b4e8128b3ca7ee75d7c88c09b632b2c064499
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-01-03 03:38:39 -0500

    Create small `Token` strong type

--- src/auth/manager.rs
+++ src/auth/manager.rs
@@ -5,6 +5,16 @@ use chrono::prelude::*;
 use papaya::HashMap;
 use uuid::Uuid;
 
+// Simple strong type around Uuid for clarity
+#[derive(Hash, PartialEq, Eq, Clone, Copy)]
+pub struct Token(Uuid);
+
+impl Token {
+    pub fn generate() -> Self {
+        Self(Uuid::new_v4())
+    }
+}
+
 /// global variable holding the singleton instance of [AuthManager].
 pub static SESSIONS: LazyLock<AuthManager> = LazyLock::new(AuthManager::start);
 
@@ -34,13 +44,16 @@ impl AccountSession {
     }
 }
 
-/// A global authentication manager which essentially acts as an
-/// always-running service providing account registration and lookup.
+/// A global authentication manager which handles logging in users and
+/// authenticating their requests via session tokens. This struct uses
+/// UUID v4s as session tokens, which are issued upon a successful login and
+/// stored in the session map. A user authenticates themselves per-action
+/// by providing their session token along with their username.
 pub struct AuthManager {
     /// A hash table mapping session IDs to their respective account records.
-    sessions: Arc<HashMap<Uuid, AccountSession>>,
+    sessions: Arc<HashMap<Token, AccountSession>>,
     /// A hash table mapping logged-in users' names to their session IDs.
-    session_ids: Arc<HashMap<String, Uuid>>,
+    session_ids: Arc<HashMap<String, Token>>,
 }
 
 // Mark types as safe to send since all methods use thread-safe
@@ -58,16 +71,25 @@ impl AuthManager {
     }
 
     // Methods //
-    pub fn register_new_session(&self, session: AccountSession) {
-        let uuid: Uuid = uuid::Uuid::new_v4();
-        self.sessions.clone().pin().insert(uuid, session);
+    /// Registers a given [AccountSession] into the global session table
+    /// by generating a new [Token] and returning it.
+    ///
+    /// TODO:
+    /// Check if account is already logged in, and if it is (and its session has
+    /// not expired), return None.
+    pub fn register_new_session(&self, session: AccountSession) -> Option<Token> {
+        let token: Token = Token::generate();
+        self.sessions.clone().pin().insert(token, session);
+        Some(token)
     }
 
-    pub fn authenticate(&self, username: &str, uuid: &Uuid) -> bool {
+    /// Attempts to authenticate a user's credentials by ensuring they have the
+    /// correct session token for their username.
+    pub fn authenticate(&self, username: &str, token: &Token) -> bool {
         self.session_ids
             .clone()
             .pin()
             .get(username)
-            .is_some_and(|u| u == uuid)
+            .is_some_and(|u| u == token)
     }
 }
