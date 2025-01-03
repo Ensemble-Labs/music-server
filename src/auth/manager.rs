c0e9c55941865384d2ed7e35d2434fbe7cbe3244
Tahir Chaudhry <ubuntuhasnoroblox@gmail.com>
2025-01-03 00:51:17 -0500

    Rewrite authentication manager to match new singleton structure

--- src/auth/manager.rs
+++ src/auth/manager.rs
@@ -1,21 +1,67 @@
-use std::sync::LazyLock;
+use std::sync::{Arc, LazyLock};
 
+use crate::types::AccountRecord;
+use chrono::prelude::*;
 use dashmap::DashMap;
 use uuid::Uuid;
 
-/// Global state holding a list of all current connected sessions
-pub static SESSIONS: LazyLock<DashMap<Uuid, AccountSession>> = LazyLock::new(DashMap::new);
+/// global state holding a list of all current connected sessions
+pub static SESSIONS: LazyLock<AuthManager> = LazyLock::new(AuthManager::start);
 
-pub struct AccountSession;
+/// Holds information about a logged in account during its
+/// current session.
+pub struct AccountSession {
+    record: AccountRecord,
+    started: DateTime<Utc>,
+    expires: DateTime<Utc>,
+}
 
-/// A global authentication manager which essentially
+impl AccountSession {
+    pub fn record(&self) -> &AccountRecord {
+        &self.record
+    }
+
+    pub fn started(&self) -> DateTime<Utc> {
+        self.started
+    }
+
+    pub fn expires(&self) -> DateTime<Utc> {
+        self.expires
+    }
+}
+
+/// a global authentication manager which essentially
 /// acts as an always-running service providing account
 /// registration and lookup.
-pub struct AuthManager;
+pub struct AuthManager {
+    sessions: Arc<DashMap<Uuid, AccountSession>>,
+    session_ids: Arc<DashMap<String, Uuid>>,
+}
+
+// Mark types as safe to send since all methods use thread-safe
+// operations (operating on [Arc]s)
+unsafe impl Send for AuthManager {}
+unsafe impl Sync for AuthManager {}
 
 impl AuthManager {
-    pub fn register_new_session(session: AccountSession) {
+    // Constructor //
+    pub fn start() -> Self {
+        Self {
+            sessions: Arc::new(DashMap::new()),
+            session_ids: Arc::new(DashMap::new()),
+        }
+    }
+
+    // Methods //
+    pub fn register_new_session(&self, session: AccountSession) -> Option<AccountSession> {
         let uuid: Uuid = uuid::Uuid::new_v4();
-        let _old: Option<AccountSession> = SESSIONS.insert(uuid, session);
+        self.sessions.clone().insert(uuid, session)
+    }
+
+    pub fn authenticate(&self, username: &str, uuid: &Uuid) -> bool {
+        self.session_ids
+            .clone()
+            .get(username)
+            .is_some_and(|u| u.value() == uuid)
     }
 }
